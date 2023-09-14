c6605a9cf75ba4fc6d2b0c410bf7b5bbce54a465
Dylan WRZESNIEWSKI <wrzdylan@gmail.com>
2023-09-14 15:55:45 +0200

    composer update packages + add api

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
